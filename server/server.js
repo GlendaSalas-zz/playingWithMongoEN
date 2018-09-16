4efc4c183314113d530a42c7e561ee5f63dd2edc
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-15 21:50:45 -0500

    add post user login

--- server/server.js
+++ server/server.js
@@ -103,6 +103,16 @@ app.get('/todos/:id',(req,res)=>{
   app.get('/users/me',authenticate,(req, res)=>{
     res.send(req.user);
   });
+  app.post('/users/login',(req, res)=>{
+    var body= _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email, body.password).then((user)=>{
+      return user.generateAuthToken().then((token)=>{
+        res.header('x-auth',token).send(user);
+      });
+    }).catch((e)=>{
+      res.status(404).send();
+    });
+  });
   app.listen(port,()=>{
     console.log(`Started on port ${port}`);
   });
