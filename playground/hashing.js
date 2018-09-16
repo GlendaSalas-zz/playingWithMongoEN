40fc615368ed6f3170b5ea7625c2b1163fb25c78
GlendaSalas <glenda.salas.a@gmail.com>
2018-09-15 21:21:24 -0500

    Password with hash and salt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,4 +1,14 @@
-var bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+var password='abc123';
+// bcrypt.genSalt(10,(err, salt)=>{
+//   bcrypt.hash(password, salt,(err, hash)=>{
+//     console.log(hash);
+//   });
+// });
+var hashPassword='$2a$10$JKvQHfn4kzH1ek/i5E/EtuqeF99pdAaxv6JVHhvPtS/4HptyPMwRO';
+bcrypt.compare(password,hashPassword,(err, res)=>{
+  console.log(res);
+});
 // const saltRounds = 10;
 // const myPlaintextPassword = 'hola mundo';
 // const someOtherPlaintextPassword = 'not_bacon';
@@ -11,12 +21,12 @@ var bcrypt = require('bcrypt');
 // bcrypt.compare(myPlaintextPassword, hash_, function(err, res) {
 //     console.log(res);
 // });
-
-const jwt= require('jsonwebtoken');
-var data={
-  id:10
-}
-var token=jwt.sign(data,'123abc');
-console.log(token);
-var decode=jwt.verify(token,'123abc');
-console.log(decode);
+//
+// const jwt= require('jsonwebtoken');
+// var data={
+//   id:10
+// }
+// var token=jwt.sign(data,'123abc');
+// console.log(token);
+// var decode=jwt.verify(token,'123abc');
+// console.log(decode);
